b958e50f6413500011e75a712c5ab2cd5284b9ae
diegomdomi <diegomdomi@gmail.com>
2022-10-22 16:44:57 +0200

    component folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import LoginStore from '../src/Components/FakeStore/LoginStore.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          React Project Documentation
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoginStore></LoginStore>
   );
 }
 
